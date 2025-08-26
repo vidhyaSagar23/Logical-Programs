2099902ab9ffa1184720361a1445c1eee52fe2c2
vidhyaSagar23 <vidhyasagar2k01@gmail.com>
2025-08-26 23:08:47 +0530

    Completed Reverse Number program

--- src/main/java/com/logicals/numbers/ReverseNumber.java
+++ src/main/java/com/logicals/numbers/ReverseNumber.java
@@ -1,5 +1,17 @@
 package com.logicals.numbers;
 
 public class ReverseNumber {
+    public static void main(String[] args) {
+        System.out.println(reverseNum(123));;
+    }
 
+    private static int reverseNum(int n) {
+        int res = 0;
+        while(n != 0){
+            int temp = n % 10;
+            res = res * 10 + temp;
+            n /= 10;
+        }
+        return res;
+    }
 }
