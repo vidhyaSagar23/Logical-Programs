fed9548779653d15bf6c3dc7e33e944acd34cb16
vidhyaSagar23 <vidhyasagar2k01@gmail.com>
2025-08-26 23:20:22 +0530

    Completed Sum of digits program

---
+++ src/main/java/com/logicals/numbers/SumOfDigits.java
@@ -0,0 +1,16 @@
+package com.logicals.numbers;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println(sumDigits(554321));;
+    }
+
+    private static int sumDigits(int n) {
+        int sum = 0;
+        while(n != 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
